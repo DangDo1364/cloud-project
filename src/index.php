8c7c95b98c37e2c3a9b920216d7b31910d369866
DangDo1364 <thaykv848565@gmail.com>
2023-06-03 14:45:36 +0700

    first commit

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Hello from the docker yooooo container";
+
+$mysqli = new mysqli("beliy-db.mysql.database.azure.com", "dangdo", 
+"01259977014Do@", "banxe");
+
+$sql = 'SELECT * FROM taikhoan';
+
+if ($result = $mysqli->query($sql)) {
+    while ($data = $result->fetch_object()) {
+        $users[] = $data;
+    }
+}
+
+foreach ($users as $user) {
+    echo "<br>";
+    echo $user->username . " " . $user->password;
+    echo "<br>";
+}
