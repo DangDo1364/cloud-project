7ba3da7081a6f70514d2104bc8845348a09203c1
tlinh212 <phamthitruclinh212@gmail.com>
2023-06-04 01:29:11 +0700

    add admin.php

--- src/login.php
+++ src/login.php
@@ -41,11 +41,11 @@ if(!empty($_POST))
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title> MOWO - MOTO WORLD </title> 
-        <link href="CSS/styles.css" rel="stylesheet" />
+        <link href="../CSS/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="JS/scripts.js"></script>
+        <script src="../JS/scripts.js"></script>
     </head>
     
     <body class="bg-primary">
