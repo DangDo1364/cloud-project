5fae09ae94399ca323f1bb11c49f22ed4b2b618a
tlinh212 <phamthitruclinh212@gmail.com>
2023-06-07 01:06:52 +0700

    u

--- src/handleLogin.php
+++ src/handleLogin.php
@@ -1,52 +1,21 @@
 <?php
 
-    $mysqli = new mysqli("beliy-db.mysql.database.azure.com", "dangdo", 
-    "01259977014Do@", "banxe");
+echo "Hello from the docker yooooo container";
 
-    $sql = 'SELECT * from taikhoan where username = '$username' and password = '$password'';    
-    $result = $mysqli->query($sql);
-        if ($result-> num_rows==0) 
-        {
-            header('Location: login.php');
-            echo "Not found";
-            // header("Location: login.php?error=Not found");          
-        }
-        else
-        {
-            header('Location: admin.php');
-        }
-    // require_once ('../config/config.php');
-    // $username = '123';
-    // $password = '';
-    // check if isset username and password
-    // if(isset($_POST['username']) && isset($_POST['password']))
-    // {   
-    //     echo 1;
-    //     $username = $_POST['username'];
-    //     $password = $_POST['password'];
-    // }
-    // if(!empty($username) && !empty($password))
-    // {
-    //     echo 2;
-    //     $con = mysqli_connect(HOST,USERNAME,PASSWORD,DATABASE);
-    //     if ($con->connect_error) {
-    //         echo 3;
-    //         echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    //         exit();
-    //     }
-    //     // $sql = 'SELECT * from taikhoan where username = '$username' and password = '$password'';
-    //     // $query = mysqli_query($con,$sql);
-    //     // echo "Returned rows are: " . $query->num_rows;
-    //     // $num_rows = mysqli_num_rows($query);
-    //     // if ($num_rows==0) 
-    //     // {
-    //     //     header('Location: admin.php');
-    //     //     echo "Not found";
-    //     //     // header("Location: login.php?error=Not found");          
-    //     // }
-    //     // else
-    //     // {
-    //     //     header('Location: admin.php');
-    //     // }
-    // }
+$mysqli = new mysqli("beliy-db.mysql.database.azure.com", "dangdo", 
+"01259977014Do@", "banxe");
+
+$sql = "SELECT * FROM taikhoan where username = 'dangdo' and password = '123456'";
+
+if ($result = $mysqli->query($sql)) {
+    echo "Returned rows are: " . $result -> num_rows;
+    // Free result set
+    $result -> free_result();
+}
+
+foreach ($users as $user) {
+    echo "<br>";
+    echo $user->username . " " . $user->password;
+    echo "<br>";
+}
 ?>
--- src/login.php
+++ src/login.php
@@ -27,7 +27,7 @@
                                     </div>
                                     <div class="card-body">
                                         
-                                        <form action="./handleLogin.php" method="post">
+                                        <form action="handleLogin.php" method="post">
                                             <div class="form-group">
                                                 <label class="small mb-1" for="inputEmailAddress"> TÀI KHOẢN </label>
                                                 <input class="form-control py-4" id="username" name="username" type="text"/>
