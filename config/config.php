335cb8ddd2a922bb9dea7c4d1f556f53942f39c8
tlinh212 <phamthitruclinh212@gmail.com>
2023-06-07 00:26:30 +0700

    update

--- config/config.php
+++ config/config.php
@@ -3,3 +3,6 @@ define('HOST','beliy-db.mysql.database.azure.com');
 define('USERNAME','dangdo');
 define('PASSWORD','01259977014Do@');
 define('DATABASE','banxe');
+    $username = '';
+    $password = '';
+    ?>
