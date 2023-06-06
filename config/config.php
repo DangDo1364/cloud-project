0002613d62e0c20dbf3f96f8f3b294e7b7d7647c
tlinh212 <phamthitruclinh212@gmail.com>
2023-06-07 00:28:11 +0700

    update

--- config/config.php
+++ config/config.php
@@ -3,6 +3,31 @@ define('HOST','beliy-db.mysql.database.azure.com');
 define('USERNAME','dangdo');
 define('PASSWORD','01259977014Do@');
 define('DATABASE','banxe');
-    $username = '';
+$username = '';
     $password = '';
+    if(isset($_POST['username']) && isset($_POST['password']))
+    {   
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+    }
+    if(!empty($username) && !empty($password))
+    {
+        $con = mysqli_connect(HOST,USERNAME,PASSWORD,DATABASE);
+        if ($con -> connect_error) {
+            echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+            exit();
+        }
+        $sql = 'SELECT * from taikhoan where username = '$username' and password = '$password'';
+	    $query = mysqli_query($con,$sql);
+        echo "Returned rows are: " . $query->num_rows;
+	    $num_rows = mysqli_num_rows($query);
+	    if ($num_rows==0) 
+        {
+            header("Location: login.php?error=Not found");          
+        }
+        else
+        {
+            header('Location: admin.php');
+        }
+    }
     ?>
--- src/login.php
+++ src/login.php
@@ -2,33 +2,7 @@
     require_once ('../config/config.php');
     echo 123;
     die();
-    // $username = '';
-    // $password = '';
-    // if(isset($_POST['username']) && isset($_POST['password']))
-    // {   
-    //     $username = $_POST['username'];
-    //     $password = $_POST['password'];
-    // }
-    // if(!empty($username) && !empty($password))
-    // {
-    //     $con = mysqli_connect(HOST,USERNAME,PASSWORD,DATABASE);
-    //     if ($con -> connect_error) {
-    //         echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    //         exit();
-    //     }
-    //     $sql = 'SELECT * from taikhoan where username = '$username' and password = '$password'';
-	//     $query = mysqli_query($con,$sql);
-    //     echo "Returned rows are: " . $query->num_rows;
-	//     $num_rows = mysqli_num_rows($query);
-	//     if ($num_rows==0) 
-    //     {
-    //         header("Location: login.php?error=Not found");          
-    //     }
-    //     else
-    //     {
-    //         header('Location: admin.php');
-    //     }
-    // }
+    
 ?>
 <!DOCTYPE html>
 <html lang="vi">
