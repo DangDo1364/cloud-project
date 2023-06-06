a2d6d906206392999542813cf71fb80d804cec42
tlinh212 <phamthitruclinh212@gmail.com>
2023-06-07 00:08:52 +0700

    update

--- src/login.php
+++ src/login.php
@@ -1,7 +1,37 @@
 <?php
-require_once ('../config/config.php');
+    echo 123;
+    die();
+    // require_once ('../config/config.php');
+    // $username = '';
+    // $password = '';
+    // die("00000000");
+    // if(isset($_POST['username']) && isset($_POST['password']))
+    // {   
+    //     $username = $_POST['username'];
+    //     $password = $_POST['password'];
+    // }
+    
+    // if(!empty($username) && !empty($password))
+    // {
+    //     $con = mysqli_connect(HOST,USERNAME,PASSWORD,DATABASE);
+    //     if ($con -> connect_error) {
+    //         echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    //         exit();
+    //     }
+    //     $sql = 'SELECT * from taikhoan where username = '$username' and password = '$password'';
+	//     $query = mysqli_query($con,$sql);
+    //     echo "Returned rows are: " . $query->num_rows;
+	//     $num_rows = mysqli_num_rows($query);
+	//     if ($num_rows==0) 
+    //     {
+    //         header("Location: login.php?error=Not found");          
+    //     }
+    //     else
+    //     {
+    //         header('Location: admin.php');
+    //     }
+    // }
 ?>
-
 <!DOCTYPE html>
 <html lang="vi">
     <head>
@@ -10,124 +40,52 @@ require_once ('../config/config.php');
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title> MOWO - MOTO WORLD </title>
+        <title> MOWO - MOTO WORLD </title> 
         <link href="../CSS/styles.css" rel="stylesheet" />
-        <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="../JS/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="../JS/chart-area-demo.js"></script>
-        <script src="../JS/chart-bar-demo.js"></script>
-        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-        <script src="../JS/datatables-demo.js"></script>
     </head>
     
-    <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-<!--Logo-->             
-           <a class="navbar-brand" href="admin.php"> Moto World </a>
-
-<!--Logout--> 
-            <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">        
-            <ul class="navbar-nav ml-auto ml-md-0">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="#"> Đổi mật khẩu </a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="logout.php"> Đăng xuất </a>
-                    </div>
-                </li>
-            </ul>
-            </form>
-
-<!--Menu--> 
-        </nav>
-        <div id="layoutSidenav">
-            <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <div class="sb-sidenav-menu-heading"> DANH MỤC </div>
-                            <a class="nav-link collapsed" href="product.jsp?id=1" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-motorcycle"></i></div>
-                                SẢN PHẨM
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                <?php
-                                        // lấy dữ liệu hãng ra
-                                        $sql = 'select * from hang';   
-                                        $categoryList = executeResult($sql);
-                                        foreach ($categoryList as $item)
-                                        {
-                                            echo '<a class="nav-link" href="product.php?tenhang='.$item['tenhang'].'">'.$item['tenhang'].'</a>';                                            
-                                        }
-                                ?>     
-                                </nav>
-                            </div> 
-                            
-                            <a class="nav-link" href="category.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-warehouse"></i></div>
-                                HÃNG XE
-                            </a>
-                            
-                            <a class="nav-link" href="donhang.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-boxes"></i></div>
-                                ĐƠN HÀNG
-                            </a>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header">
+                                        <h3 class="text-center font-weight-light my-4"> <strong> ĐĂNG NHẬP </strong> </h3>
+                                    </div>
+                                    <div class="card-body">
+                                        
+                                        <form action="login.php" method="post">
+                                            <div class="form-group">
+                                                <label class="small mb-1" for="inputEmailAddress"> TÀI KHOẢN </label>
+                                                <input class="form-control py-4" id="username" name="username" type="text"/>
+                                            </div>
+                                            
+                                            <div class="form-group">
+                                                <label class="small mb-1" for="inputPassword"> MẬT KHẨU </label>
+                                                <input class="form-control py-4" id="password" name="password" type="password"/>
+                                            </div>                                             
+                                            <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">   
+                                                <input class="btn btn-primary" type="submit" value="Login">
+                                                <a class="small" href="#"> Quên mật khẩu ?</a>
+                                            </div>
+                                        </form>
+                                        
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    
-                    <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                           <?php
-                                //nếu có session tên dangnhap thì ta thực hiện lệnh dưới
-                                if(isset($_SESSION['USER']) && $_SESSION['USER'] != NULL)
-                                {
-                                    echo $_SESSION['USER'];
-                                                    
-                                }
-                            ?>       
-                    </div>
-                </nav>
-            </div>     
-                    
-<!--Content-->                     
-            <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid">
-                        <h1 class="mt-4"> </h1>
-                        <div class="row">
-                            
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-danger text-white mb-4">
-                                    <div class="card-body"> HÃNG XE  </div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="category.php"> Xem chi tiết </a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body"> ĐƠN HÀNG </div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="donhang.php"> Xem chi tiết </a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                        </div>               
-                    </div>
                 </main>
-                
+            </div>
+            
+            <div id="layoutAuthentication_footer">
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid">
                         <div class="d-flex align-items-center justify-content-between small">
@@ -139,3 +97,4 @@ require_once ('../config/config.php');
         </div>
     </body>
 </html>
+
