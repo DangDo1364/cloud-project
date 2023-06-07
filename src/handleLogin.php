50b16c33f220d41d4b1a4c464a94bef5e98b6928
huuthang201 <thang.01yt@gmail.com>
2023-06-07 09:34:31 +0700

    d7

--- src/handleLogin.php
+++ src/handleLogin.php
@@ -13,17 +13,19 @@ if(isset($_POST['username']) && isset($_POST['password']))
 }
 
 $sql = "SELECT * FROM taikhoan where username = '$username' and password = '$password' limit 1";
-echo "d6";
+echo "d7";
 if ($result = $mysqli->query($sql)) {
     while ($data = $result->fetch_object()) {
         $users[] = $data;
     }
     if ($users == null) {
-        header("location: login.php?error=Not found", true, 301);          
+        echo "<script>window.location.href='login.php';</script>";
+        exit;
     }
     else
     {
-        header('location: admin.php', true, 301);
+        echo "<script>window.location.href='admin.php';</script>";
+        exit;
     }
 }
 
