8273bbefd0bdb7fe5cf27f1c769c620d801f9013
Nguyen Huu Thang <thangnh2001@gmail.com>
2023-06-20 23:42:50 +0700

    a7

--- src/admin.php
+++ src/admin.php
@@ -55,6 +55,86 @@
 
 <!--Menu--> 
         </nav>
-        
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading"> DANH MỤC </div>
+                            <a class="nav-link collapsed" href="product.jsp?id=1" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-motorcycle"></i></div>
+                                SẢN PHẨM
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                <?php
+                                        // lấy dữ liệu hãng ra
+                                        $sql = 'select * from hang';   
+                                        $categoryList = executeResult($sql);
+                                        foreach ($categoryList as $item)
+                                        {
+                                            echo '<a class="nav-link" href="product.php?tenhang='.$item['tenhang'].'">'.$item['tenhang'].'</a>';                                            
+                                        }
+                                ?>     
+                                </nav>
+                            </div> 
+                            
+                            <a class="nav-link" href="category.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-warehouse"></i></div>
+                                HÃNG XE
+                            </a>
+                            
+                            <a class="nav-link" href="donhang.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-boxes"></i></div>
+                                ĐƠN HÀNG
+                            </a>
+                        </div>
+                    </div>
+                    
+
+                </nav>
+            </div>     
+                    
+            <!--Content-->                     
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        <h1 class="mt-4"> </h1>
+                        <div class="row">
+                            
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-danger text-white mb-4">
+                                    <div class="card-body"> HÃNG XE  </div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="category.php"> Xem chi tiết </a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-primary text-white mb-4">
+                                    <div class="card-body"> ĐƠN HÀNG </div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="donhang.php"> Xem chi tiết </a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>               
+                    </div>
+                </main>
+                
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted"> Copyright &copy; MOWO - MOTO WORLD 2020 </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
     </body>
 </html>
