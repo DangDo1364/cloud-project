7cf8fd97c0941cd9e258aef47e07b9aeed8c32ac
tlinh212 <phamthitruclinh212@gmail.com>
2023-06-21 23:40:32 +0700

    update

--- src/add_category.php
+++ src/add_category.php
@@ -10,24 +10,7 @@
         header('Location: login.php');    
     }
 
-if(!empty($_POST))
-{
-    $name = '';
 
-    if(isset($_POST['name']))
-    {
-        $name = $_POST['name'];
-        $hinhanh = $_POST['hinhanh'];
-    }
-    
-    if(!empty($name))
-    {
-        echo $name
-        $sql = "INSERT INTO hang VALUES ("'.$name.'", "'.$hinhanh.'")";
-        execute($sql);
-        header('Location: category.php');
-    }
-}
 ?>
 
 <!DOCTYPE html>
