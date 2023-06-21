3ea4f1224da130361d0bf9c3d56741cd605e7e91
tlinh212 <phamthitruclinh212@gmail.com>
2023-06-22 00:34:38 +0700

    update

--- src/delete_category.php
+++ src/delete_category.php
@@ -8,7 +8,7 @@ if(!empty($_POST))
 {
     $tenhang = $_POST['tenhang'];
     echo $tenhang;
-    $sql = "delete from hang where tenhang = ".$tenhang."";
+    $sql = "delete from hang where tenhang = '.$tenhang.'";
     execute($sql);
     header('Location: category.php');    
 }
