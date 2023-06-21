46581afaa6568005214f59ff7833d3b7e145f73d
tlinh212 <phamthitruclinh212@gmail.com>
2023-06-21 23:36:38 +0700

    update

--- src/category.php
+++ src/category.php
@@ -9,14 +9,6 @@
     {
         header('Location: login.php');    
     }
-
-if(!empty($_POST))
-{
-    $tenhang = $_POST['tenhang'];
-    $sql = 'delete from hang where tenhang = "'.$tenhang.'"';
-    execute($sql);
-    header('Location: category.php');    
-}
 ?>
 
 <!DOCTYPE html>
