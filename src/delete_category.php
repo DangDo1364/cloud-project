112a0dc6735b983121bcc07177e42699314dafb6
tlinh212 <phamthitruclinh212@gmail.com>
2023-06-22 00:25:51 +0700

    update

--- src/delete_category.php
+++ src/delete_category.php
@@ -1,4 +1,5 @@
 <?php
+
 ob_start();
 
 require_once ('../config/dbhelper.php');
@@ -7,8 +8,8 @@ if(!empty($_POST))
 {
     $tenhang = $_POST['tenhang'];
     echo $tenhang;
-    //$sql = "DELETE FROM hang WHERE tenhang = ".$tenhang."";
-    //execute($sql);
-    //header('Location: category.php');    
+    $sql = "DELETE FROM hang WHERE tenhang = ".$tenhang."";
+    execute($sql);
+    header('Location: category.php');    
 }
 ?>
